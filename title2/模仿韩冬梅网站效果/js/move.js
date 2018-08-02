159ded54dd9e497e8fc68a6484057fa80cf4da33
little Tang <tang1151983729@qq.com>
2018-08-02 23:59:04 +0800

    Add files via upload

---
+++ title2/模仿韩冬梅网站效果/js/move.js
@@ -0,0 +1,38 @@
+	function startMove(obj,json,fn){
+		clearInterval(obj.timer);
+		obj.timer=setInterval(function(){
+			var flag = true;
+			for(var attr in json){// json是一个轻量级的数据交换格式
+				var icur = 0;
+				if(attr=='opacity'){
+					var icur=Math.round(parseFloat(getStyle(obj,attr))*100);
+				}else{
+					var icur=parseInt(getStyle(obj,attr));
+				}
+				var speed=(json[attr]-icur)/11;//缓冲运动
+				speed=speed>0?Math.ceil(speed):Math.floor(speed);
+				if(icur!=json[attr]){
+					flag=false;
+				}
+				if (attr == 'opacity') {
+						obj.style.filter = 'alpha(opacity:'+(icur+speed)+')';
+						obj.style.opacity=(icur+speed)/100;
+					} else {
+						obj.style[attr]=icur+speed+'px';
+					}
+				}
+			if(flag){
+				clearInterval(obj.timer);
+				if(fn){
+					fn();
+				}
+			}
+		},30)
+	}
+	function getStyle(obj,attr){
+		if(obj.curentStyle){
+			return curentStyle[attr];
+		}else{
+			return getComputedStyle(obj,false)[attr];
+		}
+	}
